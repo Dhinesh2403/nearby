8009f6d6b22447f9aa88dc2fa8c8eb5f1efc0015
Dhinesh2403 <dhinesdk024@gmail.com>
2026-06-19 22:20:42 +0530

    Seeding changes

--- server/src/seeds/seed.dev.js
+++ server/src/seeds/seed.dev.js
@@ -89,7 +89,6 @@ const run = async () => {
   await Provider.deleteMany({ userId: { $in: existingIds } });
   await Service.deleteMany({});
   await Review.deleteMany({});
-  console.log('🗑️   Cleared old seed data');
 
   const created = {};
   for (const u of users) {
